e5406054fbde4329f6e8b2c71e516490c7adf38d
mahdi <mahdimamashli1383@gmail.com>
2023-11-21 07:17:17 +0330

    delete img and finish project CheckValuePassword

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,46 @@
+let parameters = {
+    count:false,
+    letters : false,
+    num : false,
+    special : false
+}
+let valueCheck = document.getElementById('value-bar')
+let msg = document.getElementById('msg')
+
+function checkValue(){
+    let password = document.getElementById('password').value
+    parameters.letters = (/[\w]+/).test(password)?true:false
+    parameters.num = (/[\d]+/).test(password)?true:false
+    parameters.special = (/[!\"$%&/()=?@~`\\.\';:+=^*_-]+/).test(password)?true:false;
+    parameters.count = (password.length > 7)?true:false;
+    let lineValue = Object.values(parameters).filter(value => value)
+    valueCheck.innerHTML=""
+    for (let i in lineValue){
+        let span = document.createElement('span')
+        span.classList.add('line')
+        valueCheck.appendChild(span)
+    }
+    let spanRef = document.getElementsByClassName('line')
+    for(let i =0 ; i<spanRef.length ; i++){
+        switch (spanRef.length -1){
+            case 0:
+                spanRef[i].style.background="#ff3e36"
+                msg.textContent ="your password is very weak"
+                break
+            case 1:
+                    spanRef[i].style.background="#ff691f"
+                    msg.textContent="your password is weak"
+            case 2:
+                spanRef[i].style.background = "#ffda36"
+                msg.textContent = "your password is good"
+                break
+            case 3:
+                spanRef[i].style.background = "#0be881"
+                msg.textContent = "your password is strong"
+        }
+    }
+}
+
 function toggle() {
   let password = document.getElementById("password");
   let eye = document.getElementById("toggle");
